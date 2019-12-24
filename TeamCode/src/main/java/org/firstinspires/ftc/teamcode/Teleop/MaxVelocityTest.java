52e131ad1c5a00442753d9b0d1aa409d325f9a6d
Brandon Yan <yanjbrandon@gmail.com>
2019-12-23 18:20:46 -0800

    Max Velocity Test Update
    fixed max velocity test logic and fixed method overload issue with roadrunner
    
    Co-Authored-By: medhakini <medhakini@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MaxVelocityTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MaxVelocityTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp
@@ -26,29 +27,39 @@ public class MaxVelocityTest extends LinearOpMode {
         mtrBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         mtrBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        mtrFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        mtrFR.setDirection(DcMotorSimple.Direction.FORWARD);
+        mtrBL.setDirection(DcMotorSimple.Direction.REVERSE);
+        mtrBR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        mtrFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mtrFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mtrBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mtrBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         waitForStart();
 
         while (opModeIsActive()) {
-            currentVelocity = mtrVertical.getVelocity();
+            currentVelocity = mtrFL.getVelocity();
 
             if (currentVelocity > maxVelocity) {
                 maxVelocity = currentVelocity;
             }
 
-            /*
+
             if (gamepad1.a) {
                 mtrFL.setPower(1);
                 mtrFR.setPower(1);
                 mtrBL.setPower(1);
                 mtrBR.setPower(1);
+            } else {
+                mtrFL.setPower(0);
+                mtrFR.setPower(0);
+                mtrBL.setPower(0);
+                mtrBR.setPower(0);
             }
-            */
 
-            mtrVertical.setPower(gamepad1.right_stick_y / 2);
 
-            if (gamepad1.a) {
-
-            }
 
             telemetry.addData("current velocity", currentVelocity);
             telemetry.addData("maximum velocity", maxVelocity);
